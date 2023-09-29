cae4b5c03f96b172921e41afdd51b64096ba49b6
Karim Makhloufi <karim.makhloufi.paris@gmail.com>
2023-09-29 16:41:22 +0200

    CRUD category and ad delete on frontend

--- the-good-corner/frontend/src/components/RecentAds.tsx
+++ the-good-corner/frontend/src/components/RecentAds.tsx
@@ -4,7 +4,7 @@ import { AdCardProps } from "./AdCard";
 import DisplayAds from "./DisplayAds";
 
 const RecentAds = () => {
-  const [recentAds, setRecentAds] = useState<AdCardProps[]>([]);
+  const [recentAds, setRecentAds] = useState<AdCardProps[]>([]); // On indique que la valeur initiale est un tableau mais de type AdCardProps.
 
   useEffect(() => {
     const fetchData = async () => {
@@ -12,7 +12,10 @@ const RecentAds = () => {
         const result = await axios.get<AdCardProps[]>(
           "http://localhost:4000/ad"
         );
+
         console.log(result.data);
+        // console.log(result.data.map(el=> el.)); // Grâce au typage de typescript, on aura une autocomplétion avec le map, il a désormais l'information. 
+
         setRecentAds(result.data);
       } catch (err) {
         console.log("error", err);
