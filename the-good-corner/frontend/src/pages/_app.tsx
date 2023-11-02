902ee930be4501e1f1ee50243317ec164d9e1ef8
Davy <davyrobert90@gmail.com>
2023-11-02 21:34:16 +0100

    fix: error loading data because wrote https://localhost:4000 instead of http

--- the-good-corner/frontend/src/pages/_app.tsx
+++ the-good-corner/frontend/src/pages/_app.tsx
@@ -9,7 +9,7 @@ import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 // }
 
 const client = new ApolloClient({
-  uri: 'https://localhost:4000',
+  uri: 'http://localhost:4000',
   // uri: 'https://flyby-router-demo.herokuapp.com/',
   cache: new InMemoryCache(),
 });
