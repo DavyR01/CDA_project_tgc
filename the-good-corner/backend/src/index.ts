e101619856af07d14e3b33e1f5737e157ff7ca9d
Davy <davyrobert90@gmail.com>
2023-11-02 09:52:35 +0100

    feat: add packages typegraphQL, class-validator...

--- the-good-corner/backend/src/index.ts
+++ the-good-corner/backend/src/index.ts
@@ -2,14 +2,14 @@ import "reflect-metadata";
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 import { buildSchema } from "graphql";
-
+import dataSource from "../config/db";
 
 
 const start = async ()=> {
-//   await dataSource.initialize();
+  await dataSource.initialize();
 
   const schema = await buildSchema({
-    // resolvers: [BookResolver],
+    resolvers: [BookResolver],
     // emitSchemaFile: true // * This line generate a file schema.graph.ql automatically. it is optional.
   });
   const server = new ApolloServer({ schema });
