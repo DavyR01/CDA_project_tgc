9014f305a8848e8dad799ea0e3e25dec54fe7f5e
Davy <davyrobert90@gmail.com>
2024-01-03 15:35:20 +0100

    register a user in frontend

---
+++ the-good-corner/frontend/src/pages/register.tsx
@@ -0,0 +1,52 @@
+import { REGISTER } from "@/graphql/mutations/mutations";
+import { useQuery, useMutation } from "@apollo/client";
+
+const RegisterPage = () => {
+
+   const [
+      handleRegister,
+      { data, loading, error },
+   ] = useMutation(REGISTER);
+
+   return (
+      <div>
+         <p>Register Page</p>
+         <form
+            onSubmit={async (e) => {
+               e.preventDefault();
+               const form = e.target;
+               const formData = new FormData(form as HTMLFormElement);
+
+               // Or you can work with it as a plain object:
+               const formJson = Object.fromEntries(formData.entries());
+               console.log(formJson);
+
+               const result = await handleRegister({
+                  variables: {
+                     newUserData: formJson,
+                  },
+               });
+               console.log("result", result);
+            }}
+            className="text-field-with-button"
+         >
+            <input
+               name="email"
+               className="text-field main-search-field"
+               type="text"
+            />
+            <input
+               name="password"
+               className="text-field main-search-field"
+               type="password"
+            />
+            <button className="button button-primary">
+               Register
+            </button>
+         </form>
+      </div>
+
+   )
+}
+
+export default RegisterPage;
